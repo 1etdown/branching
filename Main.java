8e358581a9031d13070d9037f6fb40209e693bcb
1etdown <artem.ashurkin12@gmail.com>
2023-11-28 17:38:54 +0300

    Add anower new print

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
         System.out.print("Это новый вывод");
+        System.out.print("Это еще один вывод");
     }
 
 }
