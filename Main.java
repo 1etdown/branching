3a9b72e8f6666051d06501cddf1687753c557a07
1etdown <artem.ashurkin12@gmail.com>
2023-11-28 17:47:43 +0300

    Add new print

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.print("Это добавлено в ветке main");
         System.out.print("Это добавлено в ветке main");
         System.out.print("Это добавлено в ветке main");
+        System.out.print("Это добавлено в ветке main");
     }
         }
